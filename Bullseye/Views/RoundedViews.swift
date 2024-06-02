7cf53a2e62c0489561b215a76a83c2be060dbcf8
JustinSheng <justin.wang4@icloud.com>
2024-06-02 11:15:42 +0800

    [add] use strokeBorder to modify the rounded views

--- Bullseye/Views/RoundedViews.swift
+++ Bullseye/Views/RoundedViews.swift
@@ -37,6 +37,10 @@ struct RoundedImageViewStroked: View {
 			.font(.title)
 			.foregroundColor(Color("TextColor"))
 			.frame(width: 56, height: 56)
+			.overlay(
+				Circle()
+					.strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2.0)
+			)
 	}
 }
 
@@ -45,8 +49,12 @@ struct RoundedImageViewFilled: View {
 	var body: some View {
 		Image(systemName: systemName)
 			.font(.title)
-			.foregroundColor(Color("TextColor"))
+			.foregroundColor(Color("ButtonFilledTextColor"))
 			.frame(width: 56, height: 56)
+			.background(
+				Circle()
+					.fill(Color("ButtonFilledBackgroundColor"))
+			)
 	}
 }
 
